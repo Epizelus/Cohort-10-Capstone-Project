9b1e76c142b528f80f9d1dbe6d6c9ea44202ed29
Epizelus <brantaylo@gmail.com>
2025-05-02 09:14:28 -0400

    a sql script that creates a table based on a query has been added.

--- Tornado Alley/Scripts/sql/Table Generators/avgtableassemblygenerator.sql
+++ Tornado Alley/Scripts/sql/Table Generators/avgtableassemblygenerator.sql
@@ -1,4 +1,4 @@
-(SELECT ID, Name, State, Value, Anomaly, Rank, Mean FROM ks_temp_avg)
+CREATE TABLE us_anomalies_20152020 AS (SELECT ID, Name, State, Value, Anomaly, Rank, Mean FROM ks_temp_avg)
 UNION
 SELECT ID, Name, State, Value, Anomaly, Rank, Mean FROM ok_temp_avg
 UNION
